1f4d75c4a5087d43d84116b32d77b804e2662d94
rapha-thimoteo <166454493+rapha-thimoteo@users.noreply.github.com>
2024-09-05 16:23:43 -0300

    Create perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,115 @@
+export const perguntas = [
+    {
+        enunciado: "O que é mais importante para você em um carro?",
+        alternativas: [
+            {
+                texto: "A) Potência e desempenho",
+                afirmacao: [
+                    "Você é apaixonado por velocidade e potência! Gosta de carros que entregam performance e adrenalina. Para você, dirigir é uma experiência emocionante e cheia de aventura."
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "B) Conforto e espaço",
+                afirmacao: [
+                    " Para você, o carro é uma extensão do seu lar, oferecendo conforto e segurança. Gosta de veículos espaçosos e confiáveis, perfeitos para viagens longas e momentos com a família."
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+    {
+        enunciado: "Como você cuida do seu carro?",
+        alternativas: [
+            {
+                texto: "a)Foco na eficiência e no consumo de combustível",
+                afirmacao: [
+                    " Para você, o carro é uma extensão do seu lar, oferecendo conforto e segurança. Gosta de veículos espaçosos e confiáveis, perfeitos para viagens longas e momentos com a família.",
+    
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "B) Gosto de manter o interior limpo e organizado",
+                afirmacao: [
+                    "Seu foco está na estética e design. Você ama carros que chamam atenção pelo visual, e se preocupa em manter seu carro sempre impecável, tanto por dentro quanto por fora."
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Quando você compra um carro, o que te atrai mais?",
+        alternativas: [
+            {
+                texto: "A)  O custo-benefício e economia",
+                afirmacao: [
+                    " Para você, o carro é uma extensão do seu lar, oferecendo conforto e segurança. Gosta de veículos espaçosos e confiáveis, perfeitos para viagens longas e momentos com a família."
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "b)O visual e o design moderno",
+                afirmacao: [
+                    "A eficiência é a sua prioridade. Você procura sempre economizar combustível e encontrar o melhor custo-benefício. Para você, praticidade e economia estão em primeiro lugar."
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Qual é a sua estrada favorita para dirigir?",
+        alternativas: [
+            {
+                texto: "a) Ruas da cidade com trânsito leve",
+                afirmacao: [
+                    "A eficiência é a sua prioridade. Você procura sempre economizar combustível e encontrar o melhor custo-benefício. Para você, praticidade e economia estão em primeiro lugar."
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "B) Rodovias tranquilas para viagens longas",
+                afirmacao: [
+                    "Você é apaixonado por velocidade e potência! Gosta de carros que entregam performance e adrenalina. Para você, dirigir é uma experiência emocionante e cheia de aventura."
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "Como você personalizaria seu carro?",
+        alternativas: [
+            {
+                texto: "a)Trocaria as rodas e melhoraria a estética externa",
+                afirmacao: [
+                    "A eficiência é a sua prioridade. Você procura sempre economizar combustível e encontrar o melhor custo-benefício. Para você, praticidade e economia estão em primeiro lugar."
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "b)Adicionaria mais acessórios de conforto e tecnologia",
+                afirmacao: [
+                    "Seu foco está na estética e design. Você ama carros que chamam atenção pelo visual, e se preocupa em manter seu carro sempre impecável, tanto por dentro quanto por fora."
+                ],
+                proxima: 8,
+            },
+        ]
+    },
+    {
+        enunciado: "Como você reage quando seu carro apresenta problemas?",
+        alternativas: [
+            {
+                texto: "A) Tento resolver eu mesmo o problema mecânico",
+                afirmacao: [
+                    "Você é apaixonado por velocidade e potência! Gosta de carros que entregam performance e adrenalina. Para você, dirigir é uma experiência emocionante e cheia de aventura."
+                ],
+            },
+            {
+                texto: "B) Levo para o mecânico de confiança rapidamente",
+                afirmacao: [
+                    "A eficiência é a sua prioridade. Você procura sempre economizar combustível e encontrar o melhor custo-benefício. Para você, praticidade e economia estão em primeiro lugar."
+                ],
+            },
+        ]
+    },
+];
